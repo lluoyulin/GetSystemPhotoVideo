9f7cfa43a3f39695b071ee60b2f53a05fede45e0
freedom <freedom_luo@sina.com>
2015-06-02 14:26:05 +0800

    创建项目
    创建项目

---
+++ GetSystemPhotoVideo/GetSystemPhotoVideo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GetSystemPhotoVideo
+//
+//  Created by freedom on 15/6/2.
+//  Copyright (c) 2015年 freedom_luo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
